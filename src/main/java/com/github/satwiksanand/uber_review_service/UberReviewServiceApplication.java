6a7aaed26ef20209df8bc6311d14a1d3e285a5af
satwik sanand <sanandsatwik421@gmail.com>
2025-12-04 19:18:28 +0000

    set up basic java spring boot project and config

---
+++ src/main/java/com/github/satwiksanand/uber_review_service/UberReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.satwiksanand.uber_review_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberReviewServiceApplication.class, args);
+	}
+
+}
