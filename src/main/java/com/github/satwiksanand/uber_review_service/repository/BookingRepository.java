e1406ed940ece262b1ae49ccf3c1c0fc3517754e
satwik sanand <sanandsatwik421@gmail.com>
2025-12-11 11:51:30 +0530

    learnt about migrations in database

--- src/main/java/com/github/satwiksanand/uber_review_service/repository/BookingRepository.java
+++ src/main/java/com/github/satwiksanand/uber_review_service/repository/BookingRepository.java
@@ -1,11 +1,18 @@
 package com.github.satwiksanand.uber_review_service.repository;
 
 import com.github.satwiksanand.uber_review_service.models.Booking;
+import com.github.satwiksanand.uber_review_service.models.BookingReview;
 import com.github.satwiksanand.uber_review_service.models.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findAllByDriverIn(List<Driver> drivers);
+    List<Booking> findAllByDriverId(Long driverId);
+
+    @Query("SELECT b.bookingReview FROM Booking b WHERE b.id = :bookingId")
+    BookingReview findReviewByBookingId(@Param("bookingId") Long bookingId);
 }
--- src/main/java/com/github/satwiksanand/uber_review_service/repository/ReviewRepository.java
+++ src/main/java/com/github/satwiksanand/uber_review_service/repository/ReviewRepository.java
@@ -2,7 +2,17 @@ package com.github.satwiksanand.uber_review_service.repository;
 
 import com.github.satwiksanand.uber_review_service.models.BookingReview;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<BookingReview, Long> {
+    Integer countAllByRatingLessThanEqual(Integer rating);
+    List<BookingReview> findAllByRatingLessThanEqual(Integer rating);
+    List<BookingReview> findAllByCreatedAtBefore(Date createdAt);
 
+    @Query(nativeQuery = true,value = "select * from booking_review br where br.id in (select b.booking_review_id from booking b where b.id = :bookingId)")
+    BookingReview findByBooking_Id(@Param("bookingId") Long bookingId);
 }
